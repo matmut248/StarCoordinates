3092b6874324c06923ebdee0e2037bcbaef7a1bd
matmut248 <mat.muti@stud.uniroma3.it>
2020-05-31 11:37:37 +0200

    refactoring e aggiunta commenti

--- js/star.js
+++ js/star.js
@@ -4,9 +4,14 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 var origin = {"x": (width/2 - margin) ,"y":height/2};           //origin of the axes
 var updateTime = 2000;
+
+//each element of axisOrder specifies which axes delimit dials
 var axisOrder = [[".x1",".x2"],[".x2",".x3"],[".x3",".x4"],[".x4",".x5"],[".x5",".x1"]];
-var axesToAttr = {".x1" : "attr1", ".x2" : "attr2", ".x3" : "attr3", ".x4" : "attr4", ".x5" : "attr5",}
-var attrOrder = [axesToAttr[axisOrder[0][0]],axesToAttr[axisOrder[1][0]],axesToAttr[axisOrder[2][0]],axesToAttr[axisOrder[3][0]],axesToAttr[axisOrder[4][0]]]
+//map from axes to attributes
+var axesToAttr = {".x1" : "attr1", ".x2" : "attr2", ".x3" : "attr3", ".x4" : "attr4", ".x5" : "attr5",};
+//attrOrder specifies clockwise order of attributes from first to fifth axis
+var attrOrder = ["attr1","attr2","attr3","attr4","attr5"];
+//axisPos defined initial transformation of axes
 var axisPos = {first : "translate(" + origin.x + "," + margin + ")",
             second : "translate(" + origin.x + "," + origin.y +"),rotate(72),translate(0," + (-origin.y+margin) + ")",
             third : "translate(" + origin.x + "," + origin.y +"),rotate(144),translate(0," + (-origin.y+margin) + ")",
@@ -28,7 +33,7 @@ var svg = d3.select("body").append("svg")
     .attr("width", width - margin)
     .attr("height", height - margin);
 
-//set axes domain based on max value (about all attributes of all objects)
+//set axes's domain based on max value among all attributes
 function setAxesDomain(data){
     var maxValue =0;
     for (var i=0; i < data.length; i++ ){
@@ -43,31 +48,31 @@ function setAxesDomain(data){
 
 //drawing axes
 function drawAxes(){
-    svg.append("g")
+    svg.append("g") //FIRST
         .attr("class", "x1")
         .attr("transform", "translate(" + (width/2-margin) + "," + margin + ")")
         .call(x1Axis)
-        .select(".tick").attr("visibility","hidden");           //hidden first tick
-    svg.append("g")
+        .select(".tick").attr("visibility","hidden");           //hide first tick
+    svg.append("g") //SECOND
         .attr("class", "x2")
         .attr("transform", "translate(" + (width/2-margin) + "," + height/2 +"),rotate(72),translate(0," + (-height/2+margin) + ")")
         .call(x2Axis)
-        .select(".tick").attr("visibility","hidden");           //hidden first tick
-    svg.append("g")
+        .select(".tick").attr("visibility","hidden");           
+    svg.append("g") //THIRD
         .attr("class", "x3")
         .attr("transform", "translate(" + (width/2-margin) + "," + height/2 +"),rotate(144),translate(0," + (-height/2+margin) + ")")
         .call(x3Axis)
-        .select(".tick").attr("visibility","hidden")            //hidden first tick
-    svg.append("g")
+        .select(".tick").attr("visibility","hidden")
+    svg.append("g") //FOURTH
         .attr("class", "x4")
         .attr("transform", "translate(" + (width/2-margin) + "," + height/2 +"),rotate(216),translate(0," + (-height/2+margin) + ")")
         .call(x4Axis)
-        .select(".tick").attr("visibility","hidden");           //hidden first tick
-    svg.append("g")
+        .select(".tick").attr("visibility","hidden");         
+    svg.append("g") //FIFTH
         .attr("class", "x5")
         .attr("transform", "translate(" + (width/2-margin) + "," + height/2 +"),rotate(288),translate(0," + (-height/2+margin) + ")")
         .call(x5Axis)
-        .select(".tick").attr("visibility","hidden");           //hidden first tick
+        .select(".tick").attr("visibility","hidden");     
 
     //rotate tick's text
     svg.select(".x2").selectAll(".tick").select("text").attr("transform","translate(-8,-6)rotate(-72)");
@@ -75,7 +80,7 @@ function drawAxes(){
     svg.select(".x4").selectAll(".tick").select("text").attr("transform","translate(-14,4)rotate(142)");
     svg.select(".x5").selectAll(".tick").select("text").attr("transform","translate(-10,5)rotate(72)");
 
-    //draw axes label
+    //draw axes's label
     svg.select(".x1").append("text").attr("class","axisLabel").text(axesToAttr[".x1"]).attr("transform","translate(20,-10)");
     svg.select(".x2").append("text").attr("class","axisLabel").text(axesToAttr[".x2"]).attr("transform","translate(15,-15),rotate(-90)");
     svg.select(".x3").append("text").attr("class","axisLabel").text(axesToAttr[".x3"]).attr("transform","translate(-5,-35),rotate(-144)");
@@ -84,7 +89,7 @@ function drawAxes(){
 }
 
 //path to identify dials between two axes
-//second-to-fifth dials are created by rotating the first relative to origin
+//second-to-fifth dials are created rotating the first relative to origin
 function drawDials(){    
     var center =  "M" + origin.x + " " + origin.y;
     var firstDial = center + ", V 25,L"+(origin.x +origin.y -45 )+" "+(origin.y-((origin.y - 25)*0.31))+",Z";
@@ -93,22 +98,25 @@ function drawDials(){
         .attr("id","firstDial")
         .attr("class","dial")
         .attr("d",firstDial)
-        .attr("background-color","white")
+
     svg.append("path")
         .attr("id","secondDial")
         .attr("class","dial")
         .attr("d",firstDial)
         .attr("transform","rotate(72,"+origin.x+","+origin.y+")")
+
     svg.append("path")
         .attr("id","thirdDial")
         .attr("class","dial")
         .attr("d",firstDial)
         .attr("transform","rotate(144,"+origin.x+","+origin.y+")")
+
     svg.append("path")
         .attr("id","fourthDial")
         .attr("class","dial")
         .attr("d",firstDial)
         .attr("transform","rotate(216,"+origin.x+","+origin.y+")")
+
     svg.append("path")
         .attr("id","fifthDial")
         .attr("class","dial")
@@ -118,8 +126,11 @@ function drawDials(){
     svg.selectAll(".dial").attr("fill","white").attr("fill-opacity","0");
 }
 
-function reverse(data){
-    svg.select("#firstDial").on("click", function(){
+//reverse the axes by clicking on dial that they delimit it. transformation is also applied to ticks and labels.
+//after the transformation, data points are redrawn according to the new order of the axes
+function reverseAxes(data){
+    //FIRST
+    svg.select("#firstDial").on("click", function(){        
         svg.select(axisOrder[0][0]).transition().duration(updateTime)
             .attr("transform",axisPos.second)
         svg.select(axisOrder[0][0]).selectAll(".tick").select("text").transition().duration(updateTime)
@@ -142,7 +153,8 @@ function reverse(data){
         
         drawCasePoints(data,[axesToAttr[axisOrder[0][0]],axesToAttr[axisOrder[1][0]],axesToAttr[axisOrder[2][0]],axesToAttr[axisOrder[3][0]],axesToAttr[axisOrder[4][0]]])
     })
-    svg.select("#secondDial").on("click", function () {
+    //SECOND
+    svg.select("#secondDial").on("click", function () {  
         svg.select(axisOrder[1][0]).transition().duration(updateTime)
             .attr("transform",axisPos.third)
         svg.select(axisOrder[1][0]).selectAll(".tick").select("text").transition().duration(updateTime)
@@ -165,6 +177,7 @@ function reverse(data){
 
         drawCasePoints(data,[axesToAttr[axisOrder[0][0]],axesToAttr[axisOrder[1][0]],axesToAttr[axisOrder[2][0]],axesToAttr[axisOrder[3][0]],axesToAttr[axisOrder[4][0]]])
     })
+    //THIRD
     svg.select("#thirdDial").on("click", function () {
         svg.select(axisOrder[2][0]).transition().duration(updateTime)
             .attr("transform", axisPos.fourth)
@@ -188,6 +201,7 @@ function reverse(data){
 
         drawCasePoints(data,[axesToAttr[axisOrder[0][0]],axesToAttr[axisOrder[1][0]],axesToAttr[axisOrder[2][0]],axesToAttr[axisOrder[3][0]],axesToAttr[axisOrder[4][0]]])
     })
+    //FOURTH
     svg.select("#fourthDial").on("click", function () {
         svg.select(axisOrder[3][0]).transition().duration(updateTime)
             .attr("transform",axisPos.fifth)
@@ -211,6 +225,7 @@ function reverse(data){
 
         drawCasePoints(data,[axesToAttr[axisOrder[0][0]],axesToAttr[axisOrder[1][0]],axesToAttr[axisOrder[2][0]],axesToAttr[axisOrder[3][0]],axesToAttr[axisOrder[4][0]]])
     })
+    //FIFTH
     svg.select("#fifthDial").on("click",function () {
         svg.select(axisOrder[4][0]).transition().duration(updateTime)
             .attr("transform",axisPos.first)
@@ -236,6 +251,8 @@ function reverse(data){
     })
 }
 
+//draw data points according to the order of the axes
+//each data point are identified by a circle and a label
 function drawCasePoints(data, attrOrder){
     var values = [];
     for (var i=0; i < data.length; i++ ){
@@ -298,7 +315,7 @@ d3.json("data/dataset.json")
         drawAxes();
         drawCasePoints(data,attrOrder);
         drawDials();
-        reverse(data);
+        reverseAxes(data);
         
     })
     .catch(function(error) {
